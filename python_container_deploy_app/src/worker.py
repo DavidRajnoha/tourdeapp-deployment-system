8382fbbc4ca2b27516db4653ff2e68dd9515c119
David Rajnoha <drajnoha@redhat.com>
2024-02-18 09:04:42 +0100

    Removed the triage debug logging code from the worker

--- python_container_deploy_app/src/worker.py
+++ python_container_deploy_app/src/worker.py
@@ -1,31 +1,7 @@
 import redis
 import os
 from rq import Worker
-import logging
-import sys
 
-import tasks.start_tasks
-
-logging.basicConfig(level=logging.DEBUG)
-
-logging.debug(f"Current Working Directory: {os.getcwd()}")
-
-pythonpath = os.environ.get('PYTHONPATH', 'PYTHONPATH not set')
-logging.debug(f"PYTHONPATH: {pythonpath}")
-
-logging.debug(f"sys.path: {sys.path}")
-
-def log_directory_structure(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        logging.debug(f"{indent}{os.path.basename(root)}/")
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            logging.debug(f"{subindent}{f}")
-
-# Example usage
-log_directory_structure(os.getcwd())
 
 redis_host = os.getenv('REDIS_HOST', 'redis-db')
 redis_port = int(os.getenv('REDIS_PORT', 6379))
